84196f5aa76a194b45f93b93f52af8d8a61b6b8b
Iuri Birlea <yurabyrlya1@gmail.com>
2024-11-21 12:03:16 -0500

    Add Test Case to Find a Product by Category

--- tests/test_models.py
+++ tests/test_models.py
@@ -196,3 +196,15 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.available, available)
+
+      def test_find_by_category(self):
+        """It should Find Products by Category"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        category = products[0].category
+        count = len([product for product in products if product.category == category])
+        found = Product.find_by_category(category)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.category, category)
